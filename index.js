69c0ab41c958e436a54c28c972a1454f6caaf489
Stationarystation <raulytstation@outlook.com>
2023-02-05 11:10:08 -0400

    fix

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 // @ts-check
 import { Client } from "revolt.js";
-import { NodeVM } from "vm2";
+import { VM } from "vm2";
 
 const revolt = new Client();
-const nodevm = new NodeVM({
-    require: {
-        external: true,
-        root: './'
-    }
-});
+const nodevm = new VM();
 
 const PREFIX = "hb";
 
@@ -16,6 +11,8 @@ function runCode(code) {
     try {
         let result = nodevm.run(code || "");
         if (result && result.length > 0 || result) {
+            console.log(result);
+
             return result
         } else {
             return "Nothing was returned";
@@ -32,12 +29,15 @@ revolt.on("message", async (message) => {
     if (command === "run") {
         let expresion = args?.slice(1).join(" ");
 
+        /**
+         * @type {string | Array<any> | object}
+         */
         let result = runCode(expresion);
 
         message.reply({
             content: result
         })?.catch((e) => {
-            console.log("bot has failed", e.data.error.reason)
+            console.log("bot has failed", e)
         });
     }
 })
@@ -45,5 +45,5 @@ revolt.on("message", async (message) => {
 if (process.env.TYPE === "BOT") {
     revolt.loginBot(process.env.TOKEN || "");
 } else if (process.env.TYPE === "USER") {
-    revolt.login({ email: process.env.EMAIL || "", password: process.env.PASSWORD || ""});
+    revolt.login({ email: process.env.EMAIL || "", password: process.env.PASSWORD || "" });
 }
